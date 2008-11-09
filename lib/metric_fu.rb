38b812e176948a93c65f1f7b52f9c4b669d339ed
Andre Arko <andre@arko.net>
2008-11-08 18:56:23 -0800

    Add flog support (no rake task yet)

--- lib/metric_fu.rb
+++ lib/metric_fu.rb
@@ -1,14 +1,14 @@
 module MetricFu
+  TEMPLATE_DIR = File.join(File.dirname(__FILE__), "templates")
   BASE_DIRECTORY = ENV['CC_BUILD_ARTIFACTS'] || 'tmp/metric_fu'
   RAILS = File.exist?("config/environment.rb")
   
   if RAILS
-    MetricFu::CODE_DIRS = ['app', 'lib']
+    CODE_DIRS = ['app', 'lib']
   else
-    MetricFu::CODE_DIRS = ['lib']
+    CODE_DIRS = ['lib']
   end
 end
 
-require File.join(File.dirname(__FILE__), 'metric_fu', 'md5_tracker')
-require File.join(File.dirname(__FILE__), 'metric_fu', 'flog_reporter')
 require File.join(File.dirname(__FILE__), 'tasks', 'metric_fu')
+Dir[File.join(File.dirname(__FILE__), 'metric_fu/*.rb')].each{|l| require l }
