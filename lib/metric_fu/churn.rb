98d48a13d5d5fac4fe3eb790827f0488960869b4
Petrik <petrik@deheus.net>
2008-11-10 09:04:47 +0100

    Moved git and svn logic to seperate classes making it easier to implement other source control implementations

--- lib/metric_fu/churn.rb
+++ lib/metric_fu/churn.rb
@@ -1,66 +1,89 @@
 module MetricFu
   class Churn
-    class << self
-      def generate_report(output_dir, options)
-        date_range, minimum_churn_count, source_control_type = churn_options(options)
-        
-        changes = parse_log_for_changes(source_control_type, date_range)
-        changes.reject! {|file, change_count| change_count < minimum_churn_count}
-        write_churn_file(changes, output_dir)
+    
+    def initialize(options={})
+      case options[:scm]
+      when :git
+        @source_control = Git.new(options[:start_date])
+      else
+        @source_control = Svn.new(options[:start_date])
+      end       
+      @minimum_churn_count = options[:minimum_churn_count] || 5
+      @changes = parse_log_for_changes.reject! {|file, change_count| change_count < @minimum_churn_count}
+    end
+    
+    def self.generate_report(output_dir, options)
+      churn = Churn.new(options)
+      churn.write_churn_file(output_dir)
+    end
+
+    def write_churn_file(output_dir)
+      FileUtils.mkdir_p(output_dir, :verbose => false) unless File.directory?(output_dir)
+      File.open("#{output_dir}/index.html", "w+") do |file|
+        file << CHURN_FILE_BEGINING
+        @changes.to_a.sort {|x,y| y[1] <=> x[1]}.each do |change|
+          file << "<tr><td>#{change[0]}</td><td class='warning'>#{change[1]}</td></tr>\n"
+        end
+        file << CHURN_FILE_END
+      end    
+    end 
+  
+    private
+    
+    def parse_log_for_changes
+      changes = {}
+      
+      logs = @source_control.get_logs
+      logs.each do |line|
+        changes[line] ? changes[line] += 1 : changes[line] = 1 
+      end    
+      changes
+    end    
+
+    
+    class SourceControl
+      def initialize(start_date=nil)
+        @start_date = start_date
       end
       
       private
+      def require_rails_env
+        require RAILS_ROOT + '/config/environment'
+      end
+    end
+    
+    class Git < SourceControl
+      def get_logs
+        `git log #{date_range} --name-only --pretty=format:`.split(/\n/).reject{|line| line == ""}          
+      end      
       
-      def parse_log_for_changes(source_control_type, date_range)
-        changes = {}
-        
-        logs = get_logs(source_control_type, date_range)
-        logs.each do |line|
-          changes[line] ? changes[line] += 1 : changes[line] = 1 
-        end
-        
-        changes
+      private      
+      def date_range
+        if @start_date
+          require_rails_env
+          "--after=#{@start_date.call.strftime('%Y-%m-%d')}"     
+        end        
       end
       
-      def get_logs(source_control_type, date_range)
-        if source_control_type == :git
-          `git log #{date_range} --name-only --pretty=format:`.split(/\n/).reject{|line| line == ""}
-        else
-          `svn log #{date_range} --verbose`.split(/\n/).map { |line| clean_up_svn_line(line) }.compact
+    end
+    
+    class Svn < SourceControl
+      def get_logs
+        `svn log #{date_range} --verbose`.split(/\n/).map { |line| clean_up_svn_line(line) }.compact
+      end    
+        
+      private      
+      def date_range
+        if @start_date
+          require_rails_env        
+          "--revision {#{@start_date.call.strftime('%Y-%m-%d')}}:{#{Time.now.strftime('%Y-%m-%d')}}"
         end
       end
       
       def clean_up_svn_line(line)
         m = line.match(/\W*[A,M]\W+(\/.*)\b/)
         m ? m[1] : nil
-      end
-      
-      def churn_options(options)
-        if options[:start_date]
-          require RAILS_ROOT + '/config/environment'
-          if options[:scm] == :git
-            date_range = "--after=#{options[:start_date].call.strftime('%Y-%m-%d')}"
-          else
-            date_range = "--revision {#{options[:start_date].call.strftime('%Y-%m-%d')}}:{#{Time.now.strftime('%Y-%m-%d')}}"
-          end
-        else
-          date_range = ""
-        end
-        minimum_churn_count = options[:minimum_churn_count] ? options[:minimum_churn_count] : 5
-        scm = options[:scm] == :git ? :git : :svn
-        return date_range, minimum_churn_count, scm
-      end
-      
-      def write_churn_file(changes, output_dir)
-        FileUtils.mkdir_p(output_dir, :verbose => false) unless File.directory?(output_dir)
-        File.open("#{output_dir}/index.html", "w+") do |file|
-          file << CHURN_FILE_BEGINING
-          changes.to_a.sort {|x,y| y[1] <=> x[1]}.each do |change|
-            file << "<tr><td>#{change[0]}</td><td class='warning'>#{change[1]}</td></tr>\n"
-          end
-          file << CHURN_FILE_END
-        end    
-      end
+      end      
     end
   
     CHURN_FILE_BEGINING = <<-EOS
--- spec/churn_spec.rb
+++ spec/churn_spec.rb
@@ -1,4 +1,5 @@
 require 'spec'
+require 'date'
 require File.dirname(__FILE__) + '/../lib/metric_fu/churn'
 include MetricFu
 
@@ -9,19 +10,28 @@ describe MetricFu::Churn do
     end
     
     it "should strip out files that have less than the min count" do
-      Churn.should_receive(:churn_options).and_return(["", 5, :git])
-      Churn.should_receive(:parse_log_for_changes).with(:git, "").and_return({"reject" => 4, "accept" => 5})
-      Churn.should_receive(:write_churn_file).with({"accept" => 5}, "output_dir")
-      Churn.generate_report("output_dir", {})
+      logs = ["accept", "accept", "accept", "reject", "reject"]
+      git_mock = mock('git')
+      git_mock.should_receive(:get_logs).and_return(logs)
+      Churn::Git.should_receive(:new).and_return(git_mock)      
+      churn = Churn.new(:scm => :git, :minimum_churn_count => 3)
+      churn.instance_variable_get(:@changes).should == {"accept"=>3} 
     end
+    
+    it "should have a default min count of 5" do
+      churn = Churn.new()
+      churn.instance_variable_get(:@minimum_churn_count).should == 5
+    end
+        
   end
   
   describe "parse_log_for_changes" do
     it "should count the changes with git" do
       logs = ["home_page/index.html", "README", "History.txt", "README", "History.txt", "README"]
-      Churn.should_receive(:get_logs).with(:git, "").and_return(logs)
-      changes = Churn.send(:parse_log_for_changes, :git, "")
-      
+      git_mock = mock('git')
+      git_mock.should_receive(:get_logs).twice.and_return(logs)
+      Churn::Git.should_receive(:new).and_return(git_mock)      
+      changes = Churn.new(:scm => :git).send(:parse_log_for_changes)
       changes["home_page/index.html"].should == 1
       changes["History.txt"].should == 2
       changes["README"].should == 3
@@ -29,35 +39,71 @@ describe MetricFu::Churn do
     
     it "should count the changes with svn" do
       logs = ["home_page/index.html", "README", "History.txt", "README", "History.txt", "README"]
-      Churn.should_receive(:get_logs).with(:svn, "").and_return(logs)
-      changes = Churn.send(:parse_log_for_changes, :svn, "")
-      
+      svn_mock = mock('svn')
+      svn_mock.should_receive(:get_logs).twice.and_return(logs)
+      Churn::Svn.should_receive(:new).and_return(svn_mock)
+      changes = Churn.new().send(:parse_log_for_changes)
       changes["home_page/index.html"].should == 1
       changes["History.txt"].should == 2
       changes["README"].should == 3
     end    
   end
+end
+
+describe MetricFu::Churn::Svn do
+  
+  describe "get_logs" do
+    it "should use the start date if supplied" do
+      @churn = Churn::Svn.new          
+      @churn.should_receive(:`).with('svn log  --verbose').and_return("")
+      @churn.get_logs
+    end
+    it "should use the start date if supplied" do
+      Time.should_receive(:now).and_return(Date.new(2001, 1, 2))
+      @churn = Churn::Svn.new(lambda{Date.new(2000, 1, 1)})      
+      @churn.should_receive(:require_rails_env)    
+      @churn.should_receive(:`).with("svn log --revision {2000-01-01}:{2001-01-02} --verbose").and_return("")
+      @churn.get_logs
+    end    
+  end
   
   describe "clean_up_svn_line" do
     it "should return nil for non matches" do
-      Churn.send(:clean_up_svn_line, "Adding Google analytics").should be_nil
-      Churn.send(:clean_up_svn_line, "A bunch of new files").should be_nil
+      Churn::Svn.new.send(:clean_up_svn_line, "Adding Google analytics").should be_nil
+      Churn::Svn.new.send(:clean_up_svn_line, "A bunch of new files").should be_nil
     end
       
     it "should strip out all but the full path" do
-      Churn.send(:clean_up_svn_line, "   A /trunk/lib/server.rb   ").should == "/trunk/lib/server.rb"
-      Churn.send(:clean_up_svn_line, "A   /trunk/lib/server.rb   ").should == "/trunk/lib/server.rb"
-      Churn.send(:clean_up_svn_line, "   A   /trunk/lib/server.rb   ").should == "/trunk/lib/server.rb"
-      Churn.send(:clean_up_svn_line, "   A /trunk/lib/server.rb").should == "/trunk/lib/server.rb"
-      Churn.send(:clean_up_svn_line, "A /trunk/lib/server.rb").should == "/trunk/lib/server.rb"
-      Churn.send(:clean_up_svn_line, "A   /trunk/lib/server.rb").should == "/trunk/lib/server.rb"
+      Churn::Svn.new.send(:clean_up_svn_line, "   A /trunk/lib/server.rb   ").should == "/trunk/lib/server.rb"
+      Churn::Svn.new.send(:clean_up_svn_line, "A   /trunk/lib/server.rb   ").should == "/trunk/lib/server.rb"
+      Churn::Svn.new.send(:clean_up_svn_line, "   A   /trunk/lib/server.rb   ").should == "/trunk/lib/server.rb"
+      Churn::Svn.new.send(:clean_up_svn_line, "   A /trunk/lib/server.rb").should == "/trunk/lib/server.rb"
+      Churn::Svn.new.send(:clean_up_svn_line, "A /trunk/lib/server.rb").should == "/trunk/lib/server.rb"
+      Churn::Svn.new.send(:clean_up_svn_line, "A   /trunk/lib/server.rb").should == "/trunk/lib/server.rb"
       
-      Churn.send(:clean_up_svn_line, "   M /trunk/lib/server.rb   ").should == "/trunk/lib/server.rb"
-      Churn.send(:clean_up_svn_line, "M   /trunk/lib/server.rb   ").should == "/trunk/lib/server.rb"
-      Churn.send(:clean_up_svn_line, "   M   /trunk/lib/server.rb   ").should == "/trunk/lib/server.rb"
-      Churn.send(:clean_up_svn_line, "   M /trunk/lib/server.rb").should == "/trunk/lib/server.rb"
-      Churn.send(:clean_up_svn_line, "M /trunk/lib/server.rb").should == "/trunk/lib/server.rb"
-      Churn.send(:clean_up_svn_line, "M   /trunk/lib/server.rb").should == "/trunk/lib/server.rb"
+      Churn::Svn.new.send(:clean_up_svn_line, "   M /trunk/lib/server.rb   ").should == "/trunk/lib/server.rb"
+      Churn::Svn.new.send(:clean_up_svn_line, "M   /trunk/lib/server.rb   ").should == "/trunk/lib/server.rb"
+      Churn::Svn.new.send(:clean_up_svn_line, "   M   /trunk/lib/server.rb   ").should == "/trunk/lib/server.rb"
+      Churn::Svn.new.send(:clean_up_svn_line, "   M /trunk/lib/server.rb").should == "/trunk/lib/server.rb"
+      Churn::Svn.new.send(:clean_up_svn_line, "M /trunk/lib/server.rb").should == "/trunk/lib/server.rb"
+      Churn::Svn.new.send(:clean_up_svn_line, "M   /trunk/lib/server.rb").should == "/trunk/lib/server.rb"
     end
+  end
+end
+
+describe MetricFu::Churn::Git do
+  
+  describe "get_logs" do
+    it "should use the start date if supplied" do
+      @churn = Churn::Git.new          
+      @churn.should_receive(:`).with('git log  --name-only --pretty=format:').and_return("")
+      @churn.get_logs
+    end
+    it "should use the start date if supplied" do
+      @churn = Churn::Git.new(lambda{Date.new(2000, 1, 1)})      
+      @churn.should_receive(:require_rails_env)    
+      @churn.should_receive(:`).with("git log --after=2000-01-01 --name-only --pretty=format:").and_return("")
+      @churn.get_logs
+    end    
   end
 end
