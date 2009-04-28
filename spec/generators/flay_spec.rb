6f26821e30f95893f9c8b7b6aadf04c20480cc89
Jake Scruggs <jake.scruggs@gmail.com>
2009-04-28 13:09:25 -0500

    Jake/Turner - added some flay generator specs

--- spec/generators/flay_spec.rb
+++ spec/generators/flay_spec.rb
@@ -1,7 +1,57 @@
 require File.dirname(__FILE__) + '/../spec_helper.rb'
 
 describe Flay do 
+  describe "emit method" do
+    before :each do
+      MetricFu::Configuration.run {|config| config.flay = { :dirs_to_flay => ['app', 'lib']  } }
+      File.stub!(:directory?).and_return(true)
+      @flay = MetricFu::Flay.new('base_dir')
+      
+    end
+    
+    it "should look at the dirs" do
+      Dir.should_receive(:[]).with(File.join("app", "**/*.rb")).and_return("path/to/app")
+      Dir.should_receive(:[]).with(File.join("lib", "**/*.rb")).and_return("path/to/lib")
+      @flay.should_receive(:`).with("flay path/to/app path/to/lib")
+      output = @flay.emit
+    end
+  end
+  
+  describe "analyze method" do
+    before :each do
+      lines = <<-HERE
+Total score (lower is better) = 246
 
+
+1) IDENTICAL code found in :or (mass*2 = 68)
+  app/controllers/link_targets_controller.rb:57
+  app/controllers/primary_sites_controller.rb:138
+
+2) Similar code found in :if (mass = 64)
+  app/controllers/primary_sites_controller.rb:75
+  app/controllers/primary_sites_controller.rb:76
+  app/controllers/primary_sites_controller.rb:88
+  app/controllers/primary_sites_controller.rb:89
+      HERE
+      MetricFu::Configuration.run {}
+      File.stub!(:directory?).and_return(true)
+      @flay = MetricFu::Flay.new('base_dir')
+      @flay.instance_variable_set(:@output, lines)
+    end
+    
+    it "should analyze and return matches" do
+      @flay.analyze.should == [ ["Total score (lower is better) = 246"],
+                                ["\n1) IDENTICAL code found in :or (mass*2 = 68)",
+                                  "app/controllers/link_targets_controller.rb:57",
+                                  "app/controllers/primary_sites_controller.rb:138"],
+                                ["2) Similar code found in :if (mass = 64)",
+                                  "app/controllers/primary_sites_controller.rb:75",
+                                  "app/controllers/primary_sites_controller.rb:76",
+                                  "app/controllers/primary_sites_controller.rb:88",
+                                  "app/controllers/primary_sites_controller.rb:89"] ]
+    end
+  end
+  
   describe "to_h method" do
             
     before :each do
