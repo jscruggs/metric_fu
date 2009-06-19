e08e7635a8ce83b3ec6dd7863efd2db73678d599
Jake Scruggs <jake.scruggs@gmail.com>
2009-06-19 07:29:19 -0500

    Jake/Jeff - adding in a template for Flurn

--- lib/generators/flurn.rb
+++ lib/generators/flurn.rb
@@ -1,17 +1,24 @@
 module MetricFu
   class Flurn < Generator
+    def emit
+    end
+    
     def analyze
       report_hash = MetricFu.report.report_hash
-      flurn_hash = {}
+      @flurn_hash = {}
       report_hash[:churn][:changes].each do |churn_file|
-        flurn_hash[churn_file[:file_path]] = churn_file[:times_changed]
+        @flurn_hash[churn_file[:file_path]] = churn_file[:times_changed]
       end
     
       report_hash[:flog][:pages].each do |flog_file|
-        flurn_hash[standarize_file_path(flog_file[:path])] += flog_file[:average_score]
+        @flurn_hash[standarize_file_path(flog_file[:path])] += flog_file[:average_score]
       end
     
-      flurn_hash
+      @flurn_hash
+    end
+    
+    def to_h
+      {"flurn" => @flurn_hash}
     end
   
     def standarize_file_path(file_path)
