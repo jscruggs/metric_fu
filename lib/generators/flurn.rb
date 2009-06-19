7c405f89b7a247fb21b84fa34b8e9c7f9f624123
Jake Scruggs <jake.scruggs@gmail.com>
2009-06-18 21:09:59 -0500

    Jake/Jeff - making Flurn use the report directly instead of going to the file system

--- lib/generators/flurn.rb
+++ lib/generators/flurn.rb
@@ -1,19 +1,21 @@
-class Flurn
-  def analyze
-    report_hash = YAML::load(File.open('report.yml'))
-    flurn_hash = {}
-    report_hash[:churn][:changes].each do |churn_file|
-      flurn_hash[churn_file[:file_path]] = churn_file[:times_changed]
-    end
+module MetricFu
+  class Flurn < Generator
+    def analyze
+      report_hash = MetricFu.report.report_hash
+      flurn_hash = {}
+      report_hash[:churn][:changes].each do |churn_file|
+        flurn_hash[churn_file[:file_path]] = churn_file[:times_changed]
+      end
     
-    report_hash[:flog][:pages].each do |flog_file|
-      flurn_hash[standarize_file_path(flog_file[:path])] += flog_file[:average_score]
-    end
+      report_hash[:flog][:pages].each do |flog_file|
+        flurn_hash[standarize_file_path(flog_file[:path])] += flog_file[:average_score]
+      end
     
-    flurn_hash
-  end
+      flurn_hash
+    end
   
-  def standarize_file_path(file_path)
-    file_path.sub(/^\//, '')
+    def standarize_file_path(file_path)
+      file_path.sub(/^\//, '')
+    end
   end
 end
--- spec/generators/flurn_spec.rb
+++ spec/generators/flurn_spec.rb
@@ -2,8 +2,11 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Flurn do  
   it "should compute the flurn for multiple files" do
-    f = Flurn.new
-    File.should_receive(:open).and_return(easy_yaml)
+    MetricFu::Configuration.run {}
+    File.stub!(:directory?).and_return(true)
+
+    f = MetricFu::Flurn.new
+    MetricFu.should_receive(:report).and_return(mock('Report', :report_hash => churn_and_flog_hash))
     flurn_hash = f.analyze
     flurn_hash['app/controllers/primary_sites_controller.rb'].should == 41 + 13.8
     flurn_hash['app/models/link_target.rb'].should == 38 + 29.8
@@ -11,29 +14,44 @@ describe Flurn do
   end
 end
 
-def easy_yaml
-  <<-HERE
-:churn: 
-  :changes: 
-  - :times_changed: 43
-    :file_path: app/models/user.rb
-  - :times_changed: 41
-    :file_path: app/controllers/primary_sites_controller.rb
-  - :times_changed: 38
-    :file_path: app/models/link_target.rb
-:flog: 
-  :pages: 
-  - :highest_score: 40.5
-    :path: /app/controllers/primary_sites_controller.rb
-    :score: 247.8
-    :average_score: 13.8
-  - :highest_score: 70.5
-    :path: /app/models/user.rb
-    :score: 23.5
-    :average_score: 10.0
-  - :highest_score: 22.2
-    :path: /app/models/link_target.rb
-    :score: 300.1
-    :average_score: 29.8
-  HERE
+def churn_and_flog_hash
+  {:churn => {
+    :changes => [
+                  {
+                    :file_path => "app/models/user.rb",
+                    :times_changed => 43},
+                  {
+                    :file_path => "app/controllers/primary_sites_controller.rb",
+                    :times_changed => 41
+                  },
+                  {
+                    :file_path => "app/models/link_target.rb",
+                    :times_changed => 38
+                  }
+                ]
+            },
+    :flog => {
+              :pages => [
+                          {
+                            :path => "/app/controllers/primary_sites_controller.rb",
+                            :highest_score => 40.5,
+                            :score => 247.8,
+                            :average_score => 13.8
+                          },
+                          {
+                            :path=>"/app/models/user.rb",
+                            :highest_score => 70.5,
+                            :score=>23.5,
+                            :average_score=>10.0
+                          },
+                          {
+                            :path=>"/app/models/link_target.rb",
+                            :highest_score=>22.2,
+                            :score=>300.1,
+                            :average_score=>29.8
+                          }
+                        ]
+            }
+  }
+  
 end
