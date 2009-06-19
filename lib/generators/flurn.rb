b964b3c2c5e323c095378516a68106fe06bc6c6f
Jake Scruggs <jake.scruggs@gmail.com>
2009-06-18 20:21:35 -0500

    Jake/Jeff - Flurn now processes the yaml and produces a hash

--- lib/generators/flurn.rb
+++ lib/generators/flurn.rb
@@ -1,6 +1,19 @@
 class Flurn
   def analyze
-    File.open
-    {'primary_sites_controller' => 54.8}
+    report_hash = YAML::load(File.open('report.yml'))
+    flurn_hash = {}
+    report_hash[:churn][:changes].each do |churn_file|
+      flurn_hash[churn_file[:file_path]] = churn_file[:times_changed]
+    end
+    
+    report_hash[:flog][:pages].each do |flog_file|
+      flurn_hash[standarize_file_path(flog_file[:path])] += flog_file[:average_score]
+    end
+    
+    flurn_hash
+  end
+  
+  def standarize_file_path(file_path)
+    file_path.sub(/^\//, '')
   end
 end
--- spec/generators/flurn_spec.rb
+++ spec/generators/flurn_spec.rb
@@ -1,12 +1,13 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe Flurn do
-  it "should description" do
-     f = Flurn.new
-     # YAML::load(File.open('report.yml'))
-     File.should_receive(:open).and_return(easy_yaml)
-     flurn_hash = f.analyze
-     flurn_hash['primary_sites_controller'].should == 41 + 13.8
+describe Flurn do  
+  it "should compute the flurn for multiple files" do
+    f = Flurn.new
+    File.should_receive(:open).and_return(easy_yaml)
+    flurn_hash = f.analyze
+    flurn_hash['app/controllers/primary_sites_controller.rb'].should == 41 + 13.8
+    flurn_hash['app/models/link_target.rb'].should == 38 + 29.8
+    flurn_hash['app/models/user.rb'].should == 43 + 10.0
   end
 end
 
@@ -15,17 +16,24 @@ def easy_yaml
 :churn: 
   :changes: 
   - :times_changed: 43
-    :file_path: app/views/primary_sites/show.html.haml
+    :file_path: app/models/user.rb
   - :times_changed: 41
     :file_path: app/controllers/primary_sites_controller.rb
   - :times_changed: 38
     :file_path: app/models/link_target.rb
-  - :times_changed: 30
 :flog: 
   :pages: 
   - :highest_score: 40.5
     :path: /app/controllers/primary_sites_controller.rb
     :score: 247.8
     :average_score: 13.8
+  - :highest_score: 70.5
+    :path: /app/models/user.rb
+    :score: 23.5
+    :average_score: 10.0
+  - :highest_score: 22.2
+    :path: /app/models/link_target.rb
+    :score: 300.1
+    :average_score: 29.8
   HERE
 end
