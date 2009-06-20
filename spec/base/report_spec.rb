c798f6510c7adb0a94306d419238d63e20fe3a9f
David Chelimsky <dchelimsky@gmail.com>
2009-06-20 04:21:25 -0500

    use expand_path to avoid multi-loading of spec helper

--- spec/base/report_spec.rb
+++ spec/base/report_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + "/../spec_helper.rb"
+require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
 
 describe MetricFu do
   
--- spec/generators/flay_spec.rb
+++ spec/generators/flay_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../spec_helper.rb'
+require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
 
 describe Flay do 
   before :each do
