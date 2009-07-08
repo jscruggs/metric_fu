35440320cdafa5a84ac0e0dcea8b43d066ed94c3
Matthew Van Horn <mattvanhorn@Ringo.local>
2009-07-09 05:59:56 +0800

    prevent rake task from being run multiple times when other rake tasks switch the environment
    Signed-off-by: Jake Scruggs <jake.scruggs@gmail.com>

--- lib/metric_fu.rb
+++ lib/metric_fu.rb
@@ -1,3 +1,4 @@
+require 'rake'
 # Load a few things to make our lives easier elsewhere.
 module MetricFu
   LIB_ROOT = File.dirname(__FILE__)
@@ -13,8 +14,11 @@ require File.join(base_dir, 'report')
 require File.join(base_dir, 'generator')
 require File.join(base_dir, 'graph')
 
-# Load the rakefile so users of the gem get the default metric_fu task
-load File.join(MetricFu::LIB_ROOT, '..', 'tasks', 'metric_fu.rake')
+# prevent the task from being run multiple times.
+unless Rake::Task.task_defined? "metrics:all"
+  # Load the rakefile so users of the gem get the default metric_fu task
+  load File.join(MetricFu::LIB_ROOT, '..', 'tasks', 'metric_fu.rake')
+end
 
 # Now load everything else that's in the directory
 Dir[File.join(base_dir, '*.rb')].each{|l| require l }
