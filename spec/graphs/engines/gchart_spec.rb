1c9bb5fc8b005d6dce060635fb368327f05c1e42
Jake Scruggs <jake.scruggs@gmail.com>
2010-01-09 13:21:34 -0600

    fixing a spec for google graphs

--- spec/graphs/engines/gchart_spec.rb
+++ spec/graphs/engines/gchart_spec.rb
@@ -1,15 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + "/../../spec_helper")
 
 describe "Gchart graphers responding to #graph!" do
-  it "should write chart file" do
-    MetricFu.configuration
-    graphs = {}
-    MetricFu::AVAILABLE_GRAPHS.each do |graph|
-      graphs[graph] = MetricFu.const_get("#{graph.to_s.capitalize}GchartGrapher").new
-    end
-    graphs.each do |key, val|
-      val.graph!
-      lambda{ File.open(File.join(MetricFu.output_directory, "#{key.to_s.downcase}.png")) }.should_not raise_error
+  MetricFu::AVAILABLE_GRAPHS.each do |metric|
+    it "should write chart file for #{metric}" do
+      MetricFu.configuration
+      grapher = MetricFu.const_get("#{metric.to_s.capitalize}GchartGrapher").new
+      grapher.flog_average, grapher.top_five_percent_average = [7.0],[20.0] if metric == :flog #googlecharts gem has problems with [[],[]] as data
+      grapher.graph!
+      lambda{ File.open(File.join(MetricFu.output_directory, "#{metric.to_s.downcase}.png")) }.should_not raise_error
     end
   end
 end
