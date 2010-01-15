863bac3f31efc2eca0df7dc102ca5223d55c20b7
Édouard Brière <edouard.briere@gmail.com>
2010-01-15 15:28:10 +0100

    Vendor Saikuro as a gem dependency

--- lib/generators/saikuro.rb
+++ lib/generators/saikuro.rb
@@ -3,17 +3,13 @@ module MetricFu
   class Saikuro < Generator
 
     def emit
-      relative_path = [File.dirname(__FILE__), '..', '..',
-                       'vendor', 'saikuro', 'saikuro.rb']
-      saikuro = File.expand_path(File.join(relative_path))
- 
       MetricFu.saikuro[:input_directory] = format_directories
  
       options_string = MetricFu.saikuro.inject("") do |options, option|
         options + "--#{option.join(' ')} "
       end
  
-      sh %{ruby "#{saikuro}" #{options_string}} do |ok, response|
+      sh %{saikuro #{options_string}} do |ok, response|
         unless ok
           puts "Saikuro failed with exit status: #{response.exitstatus}"
           exit 1
