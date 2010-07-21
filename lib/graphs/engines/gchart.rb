07e2b5085edeab1daa1edeaba4df987ce5251c8d
Chris Griego <cgriego@gmail.com>
2010-07-20 23:56:26 -0500

    Add test coverage to rails_best_practices integration and add gchart grapher. fixes #23

--- lib/graphs/engines/gchart.rb
+++ lib/graphs/engines/gchart.rb
@@ -136,4 +136,22 @@ module MetricFu
     end
   end
 
+  class RailsBestPracticesGchartGrapher < RailsBestPracticesGrapher
+    def graph!
+      determine_y_axis_scale(@rails_best_practices_count)
+      url = Gchart.line(
+        :size => GCHART_GRAPH_SIZE,
+        :title => URI.escape("Rails Best Practices: design problems"),
+        :data => self.rails_best_practices_count,
+        :bar_colors => COLORS[0..1],
+        :legend => ['Problems'],
+        :custom => "chdlp=t",
+        :max_value => @max_value,
+        :axis_with_labels => 'x,y',
+        :axis_labels => [@labels.values, @yaxis],
+        :format => 'file',
+        :filename => File.join(MetricFu.output_directory, 'rails_best_practices.png')
+      )
+    end
+  end
 end
---
+++ spec/graphs/rails_best_practices_grapher_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe RailsBestPracticesGrapher do
+  before :each do
+    @stats_grapher = MetricFu::RailsBestPracticesGrapher.new
+    MetricFu.configuration
+  end
+
+  it "should respond to rails_best_practices_count and labels" do
+    @stats_grapher.should respond_to(:rails_best_practices_count)
+    @stats_grapher.should respond_to(:labels)
+  end
+
+  describe "responding to #initialize" do
+    it "should initialise rails_best_practices_count and labels" do
+      @stats_grapher.rails_best_practices_count.should == []
+      @stats_grapher.labels.should == {}
+    end
+  end
+
+  describe "responding to #get_metrics" do
+    before(:each) do
+      @metrics = YAML::load(File.open(File.join(File.dirname(__FILE__), "..", "resources", "yml", "20090630.yml")))
+      @date = "01022003"
+    end
+
+    it "should push 100 to rails_best_practices_count" do
+      @stats_grapher.rails_best_practices_count.should_receive(:push).with(2)
+      @stats_grapher.get_metrics(@metrics, @date)
+    end
+
+    it "should update labels with the date" do
+      @stats_grapher.labels.should_receive(:update).with({ 0 => "01022003" })
+      @stats_grapher.get_metrics(@metrics, @date)
+    end
+  end
+end
