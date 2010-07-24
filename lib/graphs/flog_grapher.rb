6be94c4c8008c8893423d57ce0519f5b2ba28a2d
Chris Griego <cgriego@gmail.com>
2010-07-24 16:52:42 -0500

    Graphers now silently ignore dates with missing metric data

--- lib/graphs/flog_grapher.rb
+++ lib/graphs/flog_grapher.rb
@@ -1,24 +1,24 @@
 module MetricFu
-  
   class FlogGrapher < Grapher
-    
     attr_accessor :flog_average, :labels, :top_five_percent_average
-    
+
     def initialize
       super
       @flog_average = []
       @labels = {}
       @top_five_percent_average =[]
     end
-    
+
     def get_metrics(metrics, date)
-      @top_five_percent_average.push(calc_top_five_percent_average(metrics))
-      @flog_average.push(metrics[:flog][:average])
-      @labels.update( { @labels.size => date })
+      if metrics && metrics[:flog]
+        @top_five_percent_average.push(calc_top_five_percent_average(metrics))
+        @flog_average.push(metrics[:flog][:average])
+        @labels.update( { @labels.size => date })
+      end
     end
-    
+
     private
-    
+
     def calc_top_five_percent_average(metrics)
       methods = metrics[:flog][:pages].inject([]) {|methods, page| methods << page[:scanned_methods]}
       methods.flatten!
@@ -34,7 +34,5 @@ module MetricFu
         total_for_five_percent / number_of_methods_that_is_five_percent.to_f
       end
     end
-    
   end
-  
 end
