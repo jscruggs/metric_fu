ef263336c508518f67114a3a40fe501b4e5a6c6d
Jake Scruggs <jscruggs@backstopsolutions.com>
2010-11-12 22:33:40 -0600

    adding some comments to explain scoring strategies

--- lib/base/scoring_strategies.rb
+++ lib/base/scoring_strategies.rb
@@ -1,15 +1,15 @@
 module ScoringStrategies
 
   def percentile(ranking, item)
-    ranking.percentile(item)
+    ranking.percentile(item) # per project score percentile
   end
   
   def identity(ranking, item)
-    ranking[item]
+    ranking[item] # Use the score you got (ex flog score of 20 is not bad even if it is the top one in project)
   end
 
   def present(row)
-    1
+    1 # If present it's a one, not present it's a zero - For things like Reek that don't have a number
   end
 
   def sum(scores)
