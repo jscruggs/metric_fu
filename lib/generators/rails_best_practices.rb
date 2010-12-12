3d0fd8f66b8627520801c6814a8c518fe45ea623
dan sinclair <dj2@everburning.com>
2010-12-11 23:38:19 -0500

    cleanup escape codes on rbp data

--- lib/generators/rails_best_practices.rb
+++ lib/generators/rails_best_practices.rb
@@ -8,14 +8,26 @@ module MetricFu
     def analyze
       @matches = @output.chomp.split("\n").map{|m| m.split(" - ") }
       total = @matches.pop
+      cleanup_color_switches(total.first)
+
       2.times { @matches.pop } # ignore wiki link
       @matches.reject! {|array| array.empty? }
       @matches.map! do |match|
         file, line = match[0].split(':')
         problem = match[1]
+
+        cleanup_color_switches(file)
+        cleanup_color_switches(problem)
+
         {:file => file, :line => line, :problem => problem}
       end
       @rails_best_practices_results = {:total => total, :problems => @matches}
+    end
+
+    def cleanup_color_switches(str)
+      return if str.nil?
+      str.gsub!(%r{^\e\[3[1|2]m}, '')
+      str.gsub!(%r{\e\[0m$}, '')
     end
 
     def to_h
