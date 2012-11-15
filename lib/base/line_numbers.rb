fe791371910f4083806c54b889fc9f8646829f71
Benjamin Fleischer <github@benjaminfleischer.com>
2012-11-14 23:24:02 -0600

    BF don't put undefined variables in a rescue block

--- lib/base/line_numbers.rb
+++ lib/base/line_numbers.rb
@@ -18,7 +18,7 @@ module MetricFu
       end
     rescue Exception => e
       #catch errors for files ruby_parser fails on
-      puts "#{e.class}\t#{e.message}\t#{class_name.inspect}\t#{sexp.inspect}\t#{e.backtrace}"
+      puts "RUBY PARSE FAILURE: #{e.class}\t#{e.message}\t#{file_sexp.inspect}\t#{contents.inspect}\t#{e.backtrace}"
       @locations
     end
 
