01a663f8552943edf291a38720dd0c4ccf79fddb
Benjamin Fleischer <github@benjaminfleischer.com>
2013-03-11 01:57:46 -0500

    Moving metric-specific code out of hotspot analyzer

--- lib/metric_fu/metrics/hotspots/hotspot_analyzer.rb
+++ lib/metric_fu/metrics/hotspots/hotspot_analyzer.rb
@@ -1,6 +1,6 @@
 require File.expand_path('analysis_error', MetricFu.errors_dir)
 MetricFu.data_structures_require { 'location' }
-%w(table record grouping ranking).each do |path|
+%w(table record grouping ranking problems).each do |path|
   MetricFu.metrics_require   { "hotspots/analysis/#{path}" }
 end
 MetricFu.metrics_require   { 'hotspots/hotspot' }
@@ -87,15 +87,7 @@ module MetricFu
       sub_table = get_sub_table(item, value)
       #grouping = Ruport::Data::Grouping.new(sub_table, :by => 'metric')
       grouping = get_grouping(sub_table, :by => 'metric')
-      problems = {}
-      grouping.each do |metric, table|
-        if details == :summary || exclude_details.include?(metric)
-          problems[metric] = present_group(metric,table)
-        else
-          problems[metric] = present_group_details(metric,table)
-        end
-      end
-      problems
+      MetricFu::HotspotProblems.new(grouping, details, exclude_details).problems
     end
 
     def worst_methods(size = nil)
@@ -231,74 +223,6 @@ module MetricFu
       end
     end
 
-    # TODO: As we get fancier, the presenter should
-    # be its own class, not just a method with a long
-    # case statement
-    def present_group(metric, group)
-      occurences = group.size
-      case(metric)
-      when :reek
-        "found #{occurences} code smells"
-      when :roodi
-        "found #{occurences} design problems"
-      when :churn
-        "detected high level of churn (changed #{group[0].times_changed} times)"
-      when :flog
-        complexity = get_mean(group.column("score"))
-        "#{"average " if occurences > 1}complexity is %.1f" % complexity
-      when :saikuro
-        complexity = get_mean(group.column("complexity"))
-        "#{"average " if occurences > 1}complexity is %.1f" % complexity
-      when :flay
-        "found #{occurences} code duplications"
-      when :rcov
-        average_code_uncoverage = get_mean(group.column("percentage_uncovered"))
-        "#{"average " if occurences > 1}uncovered code is %.1f%" % average_code_uncoverage
-      else
-        raise MetricFu::AnalysisError, "Unknown metric #{metric}"
-      end
-    end
-
-    def present_group_details(metric, group)
-      occurences = group.size
-      case(metric)
-      when :reek
-        message = "found #{occurences} code smells<br/>"
-        group.each do |item|
-          type    = item.data["reek__type_name"]
-          reek_message = item.data["reek__message"]
-          message << "* #{type}: #{reek_message}<br/>"
-        end
-        message
-      when :roodi
-        message = "found #{occurences} design problems<br/>"
-        group.each do |item|
-          problem    = item.data["problems"]
-          message << "* #{problem}<br/>"
-        end
-        message
-      when :churn
-        "detected high level of churn (changed #{group[0].times_changed} times)"
-      when :flog
-        complexity = get_mean(group.column("score"))
-        "#{"average " if occurences > 1}complexity is %.1f" % complexity
-      when :saikuro
-        complexity = get_mean(group.column("complexity"))
-        "#{"average " if occurences > 1}complexity is %.1f" % complexity
-      when :flay
-        message = "found #{occurences} code duplications<br/>"
-        group.each do |item|
-          problem    = item.data["flay_reason"]
-          problem    = problem.gsub(/^[0-9]*\)/,'')
-          problem    = problem.gsub(/files\:/,' <br>&nbsp;&nbsp;&nbsp;files:')
-          message << "* #{problem}<br/>"
-        end
-        message
-      else
-        raise MetricFu::AnalysisError, "Unknown metric #{metric}"
-      end
-    end
-
     def make_table_hash(columns)
       Hash.new { |hash, key|
         hash[key] = make_table(columns)
@@ -319,13 +243,6 @@ module MetricFu
         raise ArgumentError, "Item must be :class, :method, or :file"
       end
       tables[value]
-    end
-
-    def get_mean(collection)
-      collection_length = collection.length
-      sum = 0
-      sum = collection.inject( nil ) { |sum,x| sum ? sum+x : x }
-      (sum.to_f / collection_length.to_f)
     end
 
   end
