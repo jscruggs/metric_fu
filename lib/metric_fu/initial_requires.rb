79a0ce42de079898b89470c265f67a4ff97089f4
Benjamin Fleischer <github@benjaminfleischer.com>
2013-04-16 23:22:59 -0500

    We no longer need to enforce the syck yamler

--- lib/metric_fu/initial_requires.rb
+++ lib/metric_fu/initial_requires.rb
@@ -3,15 +3,6 @@ require 'rake'
 require 'yaml'
 require 'redcard'
 
-# Psych is not defined in Ruby < 1.9
-if defined?(Psych)
-  # Syck is not available in Ruby > 1.9 or JRuby 1.9
-  if defined?(Syck) && !RedCard.check(:jruby)
-    YAML::ENGINE.yamler = 'syck'
-  end
-end
-
-
 begin
   require 'active_support'
   require 'active_support/core_ext/object/to_json'
