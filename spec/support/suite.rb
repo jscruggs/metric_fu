52514c4afac313c59e8b42d88baf2506d9adde20
Benjamin Fleischer <github@benjaminfleischer.com>
2013-05-05 23:56:36 -0500

    Add TODO item to spec cleanup_test_files support

--- spec/support/suite.rb
+++ spec/support/suite.rb
@@ -4,10 +4,12 @@ def compare_paths(path1, path2)
 end
 
 
+# TODO these directories shouldn't be written in the first place
 def cleanup_test_files
   FileUtils.rm_rf(Dir["#{MetricFu.root_dir}/foo"])
   FileUtils.rm_rf(Dir["#{MetricFu.root_dir}/is set"])
-rescue => exception
+rescue => e
+  mf_debug "Failed cleaning up test files #{e.inspect}"
 end
 
 
