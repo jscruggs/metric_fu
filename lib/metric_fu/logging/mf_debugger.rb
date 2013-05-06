ea01f1240ae638c5dc407e7369e8c7291ddcf64d
Benjamin Fleischer <github@benjaminfleischer.com>
2013-05-06 09:04:18 -0500

    Add mf_log logger

--- lib/metric_fu/logging/mf_debugger.rb
+++ lib/metric_fu/logging/mf_debugger.rb
@@ -1,24 +1,34 @@
 module MfDebugger
+  extend self
+
   class Logger
     class << self
       attr_accessor :debug_on
       @debug_on = false
     end
-  end
-
-  def self.mf_debug(msg,&block)
-    if MfDebugger::Logger.debug_on
+    def self.log(msg, &block)
       if block_given?
         block.call
       end
-      STDOUT.puts msg
+      STDOUT.puts '*'*5 + msg.to_s
+    end
+    def self.debug(msg, &block)
+      if MfDebugger::Logger.debug_on
+        if block_given?
+          log(msg,&block)
+        else
+          log(msg)
+        end
+      end
     end
   end
+
   def mf_debug(msg,&block)
-    if block_given?
-      MfDebugger.mf_debug(msg,&block)
-    else
-      MfDebugger.mf_debug(msg)
-    end
+    MfDebugger::Logger.debug(msg, &block)
   end
+
+  def mf_log(msg,&block)
+    MfDebugger::Logger.log(msg, &block)
+  end
+
 end
