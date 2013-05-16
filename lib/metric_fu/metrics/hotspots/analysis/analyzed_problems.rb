cdc96318b5a09fe667f53048201e0833cffea63a
Avdi Grimm <avdi@avdi.org>
2013-05-16 11:29:58 -0400

    Remove unnecessary else clause.

--- lib/metric_fu/metrics/hotspots/analysis/analyzed_problems.rb
+++ lib/metric_fu/metrics/hotspots/analysis/analyzed_problems.rb
@@ -46,18 +46,17 @@ module MetricFu
       sub_table = get_sub_table(item, value)
       if(sub_table.length==0)
         raise MetricFu::AnalysisError, "The #{item.to_s} '#{value.to_s}' does not have any rows in the analysis table"
+      end
+      first_row = sub_table[0]
+      case item
+      when :class
+        MetricFu::Location.get(first_row.file_path, first_row.class_name, nil)
+      when :method
+        MetricFu::Location.get(first_row.file_path, first_row.class_name, first_row.method_name)
+      when :file
+        MetricFu::Location.get(first_row.file_path, nil, nil)
       else
-        first_row = sub_table[0]
-        case item
-        when :class
-          MetricFu::Location.get(first_row.file_path, first_row.class_name, nil)
-        when :method
-          MetricFu::Location.get(first_row.file_path, first_row.class_name, first_row.method_name)
-        when :file
-          MetricFu::Location.get(first_row.file_path, nil, nil)
-        else
-          raise ArgumentError, "Item must be :class, :method, or :file"
-        end
+        raise ArgumentError, "Item must be :class, :method, or :file"
       end
     end
     # just for testing
