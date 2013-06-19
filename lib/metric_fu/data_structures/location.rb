b0ccb3cf7081ef4b2321c83fb5a330308531f559
Benjamin Fleischer <github@benjaminfleischer.com>
2013-06-18 23:07:02 -0500

    Fix wrong arguments to display_location, split off line numbers from paths

--- lib/metric_fu/data_structures/location.rb
+++ lib/metric_fu/data_structures/location.rb
@@ -2,7 +2,8 @@ module MetricFu
   class Location
     include Comparable
 
-    attr_accessor :class_name, :method_name, :file_path, :simple_method_name, :hash
+    attr_accessor :file_path, :file_name, :line_number,
+                  :class_name, :method_name, :simple_method_name, :hash
 
     def self.get(file_path, class_name, method_name)
       location = new(file_path, class_name, method_name)
@@ -16,6 +17,7 @@ module MetricFu
 
     def initialize(file_path, class_name, method_name)
       @file_path          = file_path
+      @file_name, @line_number = file_path.to_s.split(/:/)
       @class_name         = class_name
       @method_name        = method_name
       @simple_method_name = @method_name.to_s.sub(@class_name.to_s,'')
@@ -24,7 +26,7 @@ module MetricFu
 
     # TODO - we need this method (and hash accessor above) as a temporary hack where we're using Location as a hash key
     def eql?(other)
-      # REMOVED per https://github.com/jscruggs/metric_fu/pull/67/files 
+      # REMOVED per https://github.com/jscruggs/metric_fu/pull/67/files
       # [self.file_path.to_s, self.class_name.to_s, self.method_name.to_s] == [other.file_path.to_s, other.class_name.to_s, other.method_name.to_s]
       @hash == other.hash
     end
@@ -68,6 +70,8 @@ module MetricFu
 
     def finalize
       @file_path   &&= @file_path.clone
+      @file_name   &&= @file_name.clone
+      @line_number &&= @line_number.clone
       @class_name  &&= @class_name.clone
       @method_name &&= @method_name.clone
       freeze  # we cache a lot of method call results, so we want location to be immutable
@@ -76,7 +80,7 @@ module MetricFu
     private
 
     def self.strip_modules(class_or_method_name)
-      # reek reports the method with :: not # on modules like 
+      # reek reports the method with :: not # on modules like
       # module ApplicationHelper \n def signed_in?, convert it so it records correctly
       # but classes have to start with a capital letter... HACK for REEK bug, reported underlying issue to REEK
       if(class_or_method_name=~/\:\:[A-Z]/)
