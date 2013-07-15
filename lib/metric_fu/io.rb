012d927bfdc84e1b85c6780d5e8bf864669bbd5c
Robin Curry <robin.curry@vitals.com>
2013-07-15 12:40:49 -0500

    Fix indentation.

--- lib/metric_fu/io.rb
+++ lib/metric_fu/io.rb
@@ -1,28 +1,28 @@
 module MetricFu
   module Io
-      def file_for(path)
-        return nil if path.nil?
-        return path if path.respond_to?(:write)
-        file = File.open(path_relative_to_base(path), "w")
-        at_exit do
-          unless file.closed?
-            file.flush
-            file.close
-          end
+    def file_for(path)
+      return nil if path.nil?
+      return path if path.respond_to?(:write)
+      file = File.open(path_relative_to_base(path), "w")
+      at_exit do
+        unless file.closed?
+          file.flush
+          file.close
         end
-        file
       end
+      file
+    end
 
-      def dir_for(path)
-        return nil if path.nil?
-        pathname = path_relative_to_base(path)
-        FileUtils.mkdir_p(pathname) unless File.directory?(pathname)
-        pathname
-      end
+    def dir_for(path)
+      return nil if path.nil?
+      pathname = path_relative_to_base(path)
+      FileUtils.mkdir_p(pathname) unless File.directory?(pathname)
+      pathname
+    end
 
-      def path_relative_to_base(path)
-        pathname = Pathname.pwd.join(MetricFu.base_directory) # make full path relative to base directory
-        pathname.join(path)
-      end
+    def path_relative_to_base(path)
+      pathname = Pathname.pwd.join(MetricFu.base_directory) # make full path relative to base directory
+      pathname.join(path)
+    end
   end
 end
