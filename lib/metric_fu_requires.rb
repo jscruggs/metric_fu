379da3804dec67cab4fe0ca6efaf87da0a1313cd
Benjamin Fleischer <github@benjaminfleischer.com>
2013-07-18 13:03:21 -0500

    Specify gem versions in gemspec, scan for required versions

--- lib/metric_fu_requires.rb
+++ lib/metric_fu_requires.rb
@@ -9,7 +9,7 @@ module MetricFu
 
     def method_missing(method,*args,&block)
       if (gem_version = version_for(method.to_s))
-        gem_version
+        gem_version != '' ? gem_version : nil
       else
         super
       end
@@ -17,22 +17,36 @@ module MetricFu
 
     private
 
-    def ast
-      require 'ruby_parser'
-      parser = RubyParser.new
-      gemspec = File.read(File.expand_path('../../metric_fu.gemspec', __FILE__))
-      ast = parser.parse(gemspec)
-    end
+    # def ast
+    #   require 'ruby_parser'
+    #   parser = RubyParser.new
+    #   gemspec = File.read(File.expand_path('../../metric_fu.gemspec', __FILE__))
+    #   ast = parser.parse(gemspec)
+    # end
+
+    # def gems
+    #   @gems ||= ast.find{|node| node[0] == :iter}.
+    #     find{|node| node[0] == :block}.
+    #     select{|node| node[0] == :call }.
+    #     select{|node| node[2] == :add_runtime_dependency }.
+    #     map{|node| [
+    #       node[3][1].downcase.sub('metric_fu-',''),
+    #       Array(Array(node[4])[1..-1]).map{|node|node[1]}
+    #     ]}
+    # end
 
     def gems
-      @gems ||= ast.find{|node| node[0] == :iter}.
-        find{|node| node[0] == :block}.
-        select{|node| node[0] == :call }.
-        select{|node| node[2] == :add_runtime_dependency }.
-        map{|node| [
-          node[3][1].downcase.sub('metric_fu-',''),
-          Array(Array(node[4])[1..-1]).map{|node|node[1]}
-        ]}
+      @gems ||= begin
+                  gemspec = File.readlines(File.expand_path('../../metric_fu.gemspec', __FILE__))
+                  gemspec.select{|line| line =~ /dependency/}.map{|line|
+                    line.scan(/dependency\s+['"](\S+)['"][^\['"]+([^$]+)?/)
+                    gem_name = $1
+                    if gem_name
+                      gem_version = $2.to_s.chomp
+                      [gem_name.to_s.downcase.sub('metric_fu-',''), gem_version]
+                    end
+                  }.compact
+                end
     end
 
     # @return nil if no gem found
