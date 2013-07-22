c1d7696733725c9b511c01ad29e38f982f8f880c
Benjamin Fleischer <github@benjaminfleischer.com>
2013-07-22 15:19:55 -0500

    Expose RubyParser patch

--- lib/metric_fu/metrics/flog/flog.rb
+++ lib/metric_fu/metrics/flog/flog.rb
@@ -1,19 +1,6 @@
 require 'pathname'
 require 'optparse'
-class RubyParser
-  alias_method :original_process, :process
-  def process(s,f,t)
-    original_process(s,f,t)
-  rescue => e
-    if e.message =~ /wrong number of arguments/
-      original_process(s,f)
-    else
-      raise
-    end
-  end
-end
 module MetricFu
-
   class Flog < Generator
 
     def emit
