dea50fb42e6a6b71eb56f992baeb0942d1d736ae
Benjamin Fleischer <github@benjaminfleischer.com>
2013-07-25 22:36:55 -0500

    Fix roodi specs

--- spec/metric_fu/metrics/roodi/roodi_spec.rb
+++ spec/metric_fu/metrics/roodi/roodi_spec.rb
@@ -3,21 +3,15 @@ require "spec_helper"
 describe Roodi do
   describe "emit" do
     it "should add config options when present" do
-      MetricFu::Configuration.run do |config|
-        config.add_metric(:roodi)
-        config.configure_metric(:roodi,{:roodi_config => 'lib/config/roodi_config.yml', :dirs_to_roodi => []})
-      end
-      roodi = MetricFu::Roodi.new
+      options = {:roodi_config => 'lib/config/roodi_config.yml', :dirs_to_roodi => []}
+      roodi = MetricFu::Roodi.new(options)
       roodi.should_receive(:`).with(/-config=lib\/config\/roodi_config\.yml/).and_return("")
       roodi.emit
     end
 
     it "should NOT add config options when NOT present" do
-      MetricFu::Configuration.run do |config|
-        config.add_metric(:roodi)
-        config.configure_metric(:roodi,{:dirs_to_roodi => []})
-      end
-      roodi = MetricFu::Roodi.new
+      options = {:dirs_to_roodi => []}
+      roodi = MetricFu::Roodi.new(options)
       roodi.stub(:`)
       roodi.should_receive(:`).with(/-config/).never
       roodi.emit
