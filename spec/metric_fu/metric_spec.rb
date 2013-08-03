a8b3d59731442de572eb04560823202e83c69aec
Robin Curry <robin.curry@vitals.com>
2013-08-03 07:53:56 -0500

    Clean up spec with side effect.

--- spec/metric_fu/metric_spec.rb
+++ spec/metric_fu/metric_spec.rb
@@ -1,19 +1,25 @@
 require 'spec_helper'
 
 describe MetricFu::Metric do
-  it 'can have its run_options over-written' do
-    metric = MetricFu::Metric.get_metric(:flog)
-    original_options = metric.run_options.dup
-    new_options = {:foo => 'bar'}
-    metric.run_options = new_options
-    expect(original_options).to_not eq(new_options)
-    expect(metric.run_options).to eq(new_options)
+  before do
+    @metric = MetricFu::Metric.get_metric(:flog)
+    @original_options = @metric.run_options.dup
   end
-  it 'can have its run_options modified' do
-    metric = MetricFu::Metric.get_metric(:flog)
-    original_options = metric.run_options.dup
+
+  it 'can have its run_options over-written' do
     new_options = {:foo => 'bar'}
-    metric.run_options.merge!(new_options)
-    expect(metric.run_options).to eq(original_options.merge(new_options))
+    @metric.run_options = new_options
+    expect(@original_options).to_not eq(new_options)
+    expect(@metric.run_options).to eq(new_options)
+  end
+
+  it 'can have its run_options modified' do
+    new_options = {:foo => 'bar'}
+    @metric.run_options.merge!(new_options)
+    expect(@metric.run_options).to eq(@original_options.merge(new_options))
+  end
+
+  after do
+    @metric.run_options = @original_options
   end
 end
