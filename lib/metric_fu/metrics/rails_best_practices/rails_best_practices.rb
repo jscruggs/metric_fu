94d46de06ba934737b7e04c84a5d347c86729c47
calveto <todd.calvert@gmail.com>
2013-08-12 15:57:22 -0400

    Call RailsBestPractices directly to get access to the array of errors.
    We now have the URL, so we can provide a link to rails-best-pracitces.com
    By calling the lib directly we can avoid IO overhead, however we want to
    be sure that the RailsBestPractices maintains the interface we are
    expecting, since it's not a publicly advertised interface.
    I have added some specs that will error if the interface to
    RailsBestPractices changes.
    
    huh

--- lib/metric_fu/metrics/rails_best_practices/rails_best_practices.rb
+++ lib/metric_fu/metrics/rails_best_practices/rails_best_practices.rb
@@ -1,36 +1,26 @@
+require 'rails_best_practices'
 module MetricFu
   class RailsBestPractices < Generator
+
     def emit
-      command = %Q(mf-rails_best_practices --without-color .)
-      mf_debug "** #{command}"
-      @output = `#{command}`
+      mf_debug "** Rails Best Practices"
+      path = '.'
+      analyzer = ::RailsBestPractices::Analyzer.new(path, { 'silent' => true })
+      analyzer.analyze
+      @output = analyzer.errors
     end
 
     def analyze
-      @matches = @output.chomp.split("\n").map{|m| m.split(" - ") }
-      total = @matches.pop
-      cleanup_color_switches(total.first)
-
-      2.times { @matches.pop } # ignore wiki link
-      @matches.reject! {|array| array.empty? }
-      @matches.map! do |match|
-        file, line = match[0].split(':')
-        problem = match[1]
-
-        cleanup_color_switches(file)
-        cleanup_color_switches(problem)
-
-        file.gsub!(/^\.\//, '')
-
-        {:file => file, :line => line, :problem => problem}
+      @problems = @output.collect do |problem|
+        {
+          :file    => problem.filename,
+          :line    => problem.line_number,
+          :problem => problem.message,
+          :url     => problem.url
+        }
       end
-      @rails_best_practices_results = {:total => total, :problems => @matches}
-    end
-
-    def cleanup_color_switches(str)
-      return if str.nil?
-      str.gsub!(%r{^\e\[3[1|2]m}, '')
-      str.gsub!(%r{\e\[0m$}, '')
+      total = ["Found #{@problems.count} errors."]
+      @rails_best_practices_results = {:total => total, :problems => @problems}
     end
 
     def to_h
