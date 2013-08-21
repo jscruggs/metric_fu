3df25a8c17903b37a00ba80ee0a7f94b24d29683
Benjamin Fleischer <github@benjaminfleischer.com>
2013-08-21 14:19:47 -0500

    Remove unused grouping code

--- lib/metric_fu/metrics/hotspots/analysis/grouping.rb
+++ lib/metric_fu/metrics/hotspots/analysis/grouping.rb
@@ -6,28 +6,13 @@ module MetricFu
 
     def initialize(table, opts)
       column_name = opts.fetch(:by)
-      order = opts.fetch(:order) { nil }
       hash = {}
       if column_name.to_sym == :metric # special optimized case
         hash = table.group_by_metric
       else
-        table.each do |row|
-          hash[row[column_name]] ||= MetricFu::Table.new(:column_names => row.attributes)
-          hash[row[column_name]] << row
-        end
+        raise "Unexpected column_name #{column_name}"
       end
-      if order
-        @arr = hash.sort_by(&order)
-      else
-        @arr = hash.to_a
-      end
-    end
-
-    def [](key)
-      @arr.each do |group_key, table|
-        return table if group_key == key
-      end
-      return nil
+      @arr = hash.to_a
     end
 
     def each
