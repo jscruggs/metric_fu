e76c9699c064a14caa2ff3633f06949575e5f891
Benjamin Fleischer <github@benjaminfleischer.com>
2013-09-13 16:13:01 -0500

    Make AST Node wraper for line number s-expression nodes

--- lib/metric_fu/data_structures/line_numbers.rb
+++ lib/metric_fu/data_structures/line_numbers.rb
@@ -3,6 +3,87 @@
 require 'ruby_parser'
 module MetricFu
   class LineNumbers
+    Node = Struct.new(:sexp) do
+      def nil?
+        sexp.nil?
+      end
+      def node_type
+        sexp[0]
+      end
+      def name
+        sexp[1]
+      end
+      def each_of_type(type,node_class=Node)
+        sexp.each_of_type(type) do |child_sexp|
+          yield node_class.new(child_sexp)
+        end
+      end
+      def each_module(&block)
+        each_of_type(:module,&block)
+      end
+      def each_class(&block)
+        each_of_type(:class,&block)
+      end
+      def each_singleton_class(&block)
+        each_of_type(:sclass,SingletonMethodNode,&block)
+      end
+      def each_instance_method(&block)
+        each_of_type(:defn,InstanceMethodNode,&block)
+      end
+      def each_class_method(&block)
+        each_of_type(:defs,ClassMethodNode,&block)
+      end
+      def first_line
+        sexp.line
+      end
+      def last_line
+        sexp.last.line
+      end
+      def line_range
+        (first_line..last_line)
+      end
+      def hide_methods_from_next_round(node)
+        sexp.find_and_replace_all(:defn, :ignore_me)
+        sexp.find_and_replace_all(:defs, :ignore_me)
+      end
+      def full_method_name(method_separator, class_name, module_name=nil)
+        [module_namespace(module_name), class_name, method_separator, name].join
+      end
+      def module_namespace(module_name=nil)
+        if module_name.nil?
+          nil
+        else
+          [module_name,class_method_separator].join
+        end
+      end
+      def instance_method_separator
+        '#'
+      end
+      def class_method_separator
+        '::'
+      end
+    end
+    class ClassMethodNode < Node
+      def name
+        sexp[2]
+      end
+      def full_name(module_name, class_name)
+        full_method_name(class_method_separator, class_name, module_name)
+      end
+    end
+    class InstanceMethodNode < Node
+      def full_name(module_name, class_name)
+        full_method_name(instance_method_separator, class_name, module_name)
+      end
+    end
+    class SingletonMethodNode < Node
+      def full_name(class_name)
+        full_method_name(class_method_separator, class_name)
+      end
+      def each_singleton_method(&block)
+        each_of_type(:defn,SingletonMethodNode,&block)
+      end
+    end
 
     attr_reader :file_path
 
@@ -64,53 +145,59 @@ module MetricFu
     end
 
     def process_ast(file_sexp)
-      node_name = sexp_name(file_sexp)
-      case node_name
+      node = Node.new(file_sexp)
+      case node.node_type
       when nil
         mf_log "No ruby code found in #{file_path}"
       when :class
-        process_class(file_sexp)
+        process_class(node)
       when :module
-        process_module(file_sexp)
+        process_module(node)
       else
-        mf_debug "SEXP: Parsing line numbers for classes in sexp type #{node_name.inspect}"
+        mf_debug "SEXP: Parsing line numbers for classes in sexp type #{node.node_type.inspect}"
         mf_debug "      in #{file_path}"
-        file_sexp.each_of_type(:module) { |sexp| process_class(sexp) }
-        file_sexp.each_of_type(:class)  { |sexp| process_class(sexp) }
+        node.each_module {|child_node| process_class(child_node) }
+        node.each_class  {|child_node| process_class(child_node) }
       end
     end
 
-    def sexp_name(sexp)
-      sexp[0]
-    end
-
-    def process_module(sexp)
-      module_name = sexp[1]
-      sexp.each_of_type(:class) do |exp|
-        process_class(exp, module_name)
-        hide_methods_from_next_round(exp)
+    def process_module(module_node)
+      module_name = module_node.name
+      module_node.each_class do |class_node|
+        process_class(class_node, module_name)
+        class_node.hide_methods_from_next_round
       end
-      process_class(sexp)
+      process_class(module_node)
     end
 
-    def process_class(sexp, module_name=nil)
-      class_name = sexp[1]
-      process_class_self_blocks(sexp, class_name)
-      module_name_string = module_name ? "#{module_name}::" : nil
-      sexp.each_of_type(:defn) { |s| @locations["#{module_name_string}#{class_name}##{s[1]}"] = (s.line)..(s.last.line) }
-      sexp.each_of_type(:defs) { |s| @locations["#{module_name_string}#{class_name}::#{s[2]}"] = (s.line)..(s.last.line) }
+    def process_class(class_node, module_name=nil)
+      class_name = class_node.name
+      process_singleton_methods(class_node, class_name, module_name)
+      process_instance_methods( class_node, class_name, module_name)
+      process_class_methods(    class_node, class_name, module_name)
     end
 
-    def process_class_self_blocks(sexp, class_name)
-      sexp.each_of_type(:sclass) do |sexp_in_class_self_block|
-        sexp_in_class_self_block.each_of_type(:defn) { |s| @locations["#{class_name}::#{s[1]}"] = (s.line)..(s.last.line) }
-        hide_methods_from_next_round(sexp_in_class_self_block)
+    def process_singleton_methods(class_node, class_name, module_name)
+      class_node.each_singleton_class do |singleton_node|
+        singleton_node.each_singleton_method do |singleton_method_node|
+          singleton_method_name = singleton_method_node.full_name(class_name)
+          @locations[singleton_method_name] = singleton_method_node.line_range
+        end
       end
     end
 
-    def hide_methods_from_next_round(sexp)
-      sexp.find_and_replace_all(:defn, :ignore_me)
-      sexp.find_and_replace_all(:defs, :ignore_me)
+    def process_instance_methods(class_node, class_name, module_name)
+      class_node.each_instance_method do |instance_method_node|
+        instance_method_name = instance_method_node.full_name(module_name, class_name)
+        @locations[instance_method_name] = instance_method_node.line_range
+      end
+    end
+
+    def process_class_methods(class_node, class_name, module_name)
+      class_node.each_class_method do |class_method_node|
+        class_method_name = class_method_node.full_name(module_name, class_name)
+        @locations[class_method_name] = class_method_node.line_range
+      end
     end
 
   end
