3db59d7bdeee61cfbe0e23d3666619d27a0bab9a
Benjamin Fleischer <github@benjaminfleischer.com>
2013-10-15 20:28:07 -0500

    Update spec_helper, add filters, defer gc

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,12 +21,18 @@ def mf_log(msg); mf_debug(msg); end
 Dir[MetricFu.root_dir + "/spec/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
+  config.mock_with :rspec
+
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+  config.filter_run_excluding :slow unless ENV["SLOW_SPECS"]
   config.fail_fast = ENV.include?('FAIL_FAST')
+  config.order = 'random'
+
   # :suite after/before all specs
   # :each every describe block
   # :all every it block
-
-  config.order = 'random'
 
   config.after(:suite) do
     cleanup_fs
