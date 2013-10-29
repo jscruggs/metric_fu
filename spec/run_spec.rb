974abe0a330c636bdbc1333024e84eb84baa96e3
Benjamin Fleischer <github@benjaminfleischer.com>
2013-10-29 16:39:19 -0500

    Test that the loader loads the user configuration

--- spec/run_spec.rb
+++ spec/run_spec.rb
@@ -42,7 +42,7 @@ describe MetricFu do
     it "loads the .metrics file" do
       # Global only for testing that this file gets loaded
       $metric_file_loaded = false
-      metric_fu
+      MetricFu::Run.new
       $metric_file_loaded.should be_true
     end
 
